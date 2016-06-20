d6c8b50b399905464de7b1b14fc318d597bc6660
AndresMendozaOrozco <andres.mendoza.o@gmail.com>
2016-06-19 21:46:08 -0500

    New approach using Twitter API
    php sends JSON
    Use of embeded Twitt
    Faster; Stable!

--- nowplayingsfo/php/twitterconnect.php
+++ nowplayingsfo/php/twitterconnect.php
@@ -11,36 +11,58 @@ define('CONSUMER_SECRET', 'Y49dNi2NPN9vJaPS95QnRLslOqisEuC7v934lHOfN05cVjbtDB');
 define('ACCESS_TOKEN', '2834545563-QYQqm8hnLPiU3eFyAD8SGtKhfIYW7gMp8fGh8Xd');
 define('ACCESS_TOKEN_SECRET', 'SUquQt3XC2ve3IIa8JbwMa4bsRCpZSJuCVKYAXLUTDBBT');
  
+
 // Search function definition!
-function search(array $query)
-{
+function search(array $query) {
   $toa = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
+  //return $toa->get('https://api.twitter.com/1.1/statuses/user_timeline.json?count=5&screen_name=@BInowplaying&exclude_replies=true');
   return $toa->get('search/tweets', $query);
+}
+
+// Get embeded tweet from status id
+function getEmbededTweet($statusId) {
+	$toa = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
+	//return $toa->get('statuses/oembed', array ( "url" => "https://twitter.com/Interior/status/" . $statusId , "hide_media" => "false") );
+	//$theQuery = getEmbededQuery($statusId);
+	//return $toa->get('statuses/oembed', $theQuery );
+	return $toa->get('statuses/oembed', getEmbededQuery($statusId) );
+	//return $toa->get('statuses/oembed', array ( "url" => "https://twitter.com/Interior/status/" . $statusId) );
+}
+
+function getEmbededQuery($statusId) {
+	$theQu = array(
+		"url" => "https://twitter.com/Interior/status/" . $statusId,
+		"hide_media" => "false",
+		"cards" => "true",
+		"widget_type" => "video",
+	);
+	return $theQu;
 }
  
 // Coordinates
 $localizationFilter = $_GET["localizationString"];
 // Results limit
-$countMax = 5;
+$countMax = $_GET["numresults"];
 
 // The query!
 $query = array(
   "q" => "'youtube' '#nowplaying' filter:links",
+  //"q" => "'youtube' '#nowplaying'",
   "count" => $countMax,
-  "geocode" => $localizationFilter,
+  //"geocode" => $localizationFilter,
+  "result_type" => "recent",
 );
 
-  
+// Get statuses by query
 $results = search($query);
-  
+
+$formatedresults = array();
+
 $splitChar = "\nqq";
+// Send as Ajax response: the embeded tweet and the tweet object
 foreach ($results->statuses as $result) {
-  echo 
-	$result->id_str . $splitChar . 
-	$result->user->name . $splitChar . 	
-	$result->user->profile_image_url . $splitChar . 
-	$result->user->screen_name . $splitChar . 
-	$result->text . $splitChar . 	
-	$result->created_at . $splitChar;
+	$tmpEmbededTweet = (array)(getEmbededTweet($result->id));
+	$tmpEmbededTweet["theresobjec"] = $result;
+	echo json_encode($tmpEmbededTweet) . $splitChar;
 }
 ?>
